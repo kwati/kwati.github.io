476c4b87548b68df5c81a2e0e95c238968cdf80b
acpmasquerade <dhrubastergmail.com>
2022-12-06 23:54:03 +0545

    pelican with attilia ghost cms like theme

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'Department of Technology (Khalti)'
+SITENAME = 'Khalti Engineering'
+SITEURL = ''
+THEME = 'attila-1.5'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kathmandu'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Khalti Digital Wallet', 'https://khalti.com/'),
+)
+# Social widget
+SOCIAL = ()
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+# Theme configurations 
+HOME_COVER = 'assets/images/blog_cover.png'
